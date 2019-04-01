fdeeb352ddfed9197f9d743bb76ffa1a51adb5d7
Ay Has <ahashim983@yahoo.com>
2019-04-01 23:33:21 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -17,10 +17,13 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #     :memorial_day => ["BBQ"]
   #   }
   # }
-  # return the second element in the 4th of July array
+  return holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
+  holiday_hash[:winter][:christmas] << supply
+  holiday_hash[:winter][:new_years] << supply
+
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
@@ -29,23 +32,38 @@ end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
+  holiday_hash[:spring][:memorial_day] << supply
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
 
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
+  holiday_hash[:season][:holiday_name] = supply_array
+  return holiday_hash
   # code here
   # remember to return the updated hash
 
 end
 
 def all_winter_holiday_supplies(holiday_hash)
+
+  return holiday_hash[:winter].values.flatten
+
   # return an array of all of the supplies that are used in the winter season
 
 end
 
 def all_supplies_in_holidays(holiday_hash)
+  season = ""
+  holiday = ""
+  holiday_hash.each do |season,hol|
+    season = season.to_s.capitalize+":"
+    hol.each do |arr|
+      holiday = hol.to_s.capitalize+": "+arr.flatten.to_s
+    end
+    print season+holiday
+end
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
   #   Christmas: Lights, Wreath
@@ -57,14 +75,14 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
+  arr = []
+  holiday_hash.each do |season,k|
+    if k.values.flatten.include?("BBQ")
+      arr.push(k.keys)
+    end
+  end
+  return arr.uniq.flatten
+
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
 end
-
-
-
-
-
-
-
